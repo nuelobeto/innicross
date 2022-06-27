5b448b841f16b2e650356e66914b407e6840f231
favourakpasi <akpasifavour@gmail.com>
2022-06-27 15:13:35 +0100

    update links

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,10 +2,7 @@ import "./Contact.css";
 import { useEffect } from "react";
 import { Navbar } from "./../../components/Navbar/Navbar";
 import { Footer } from "./../../components/Footer/Footer";
-import { AiFillPhone, AiFillFacebook } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { BsTwitter } from "react-icons/bs";
-// import IgSvg from "../../images/igSvg.jpg";
 
 const textLeftVariants = {
   visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: 0.5 } },
@@ -14,10 +11,6 @@ const textLeftVariants = {
 const textRightVariants = {
   visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: 0.5 } },
   hidden: { x: 200, opacity: 0 },
-};
-const titleVariants = {
-  visible: { y: 0, opacity: 1, transition: { duration: 0.5, delay: 0.3 } },
-  hidden: { y: -200, opacity: 0 },
 };
 const imgVariants = {
   visible: { scale: 1, transition: { duration: 0.5, delay: 1 } },
@@ -37,13 +30,7 @@ export const Contact = () => {
 
       <main>
         <section className="contact">
-          <motion.h2
-          // initial="hidden"
-          // whileInView="visible"
-          // variants={titleVariants}
-          >
-            How can we help?
-          </motion.h2>
+          <h2>How can we help?</h2>
           <motion.p
             className="contact_text"
             initial="hidden"
@@ -61,23 +48,17 @@ export const Contact = () => {
           >
             Get in touch with us, send us an email anytime!
           </motion.p>
-          <motion.button
-            className="email_us"
-            initial="hidden"
-            whileInView="visible"
-            variants={imgVariants}
-          >
-            Email us
-          </motion.button>
-          <div className="call">
-            <p>Talk to our support team</p>
-            <AiFillPhone />
-          </div>
-          {/* <div className="social_media">
-            <AiFillFacebook style={{ color: "#4267B2" }} />
-            <BsTwitter style={{ color: " #00acee" }} />
-            <img src={IgSvg} alt="" className="ig" />
-          </div> */}
+
+          <a href="mailto:innicrosscommerceandtradltd@gmail.com">
+            <motion.button
+              className="email_us"
+              initial="hidden"
+              whileInView="visible"
+              variants={imgVariants}
+            >
+              Email us
+            </motion.button>
+          </a>
         </section>
       </main>
 
